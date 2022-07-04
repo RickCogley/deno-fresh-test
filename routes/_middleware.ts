818d13728bc100308a18e13020fa6bfd7e908a43
Rick Cogley <rick.cogley@esolia.co.jp>
2022-07-05 07:45:19 +0900

    initial http basic auth attempt

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -20,3 +20,28 @@ export async function handler(
   resp.headers.set("Permissions-Policy", "accelerometer=(), ambient-light-sensor=*, autoplay=(self), battery=(self), camera=(), cross-origin-isolated=*, fullscreen=*, geolocation=(self), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), usb=()");
   return resp;
 }
+
+const auth = req.headers.get("authorization");
+  if (!auth) {
+    return new Response(null, {
+      status: 401,
+      headers: {
+        "content-type": "text/plain",
+        "WWW-Authenticate": 'Basic realm="Access to this page"',
+      },
+    });
+  } else {
+    const [username, password] = atob(auth.slice(6)).split(":");
+    if (
+      username === Deno.env.get("SECRET_USERNAME") &&
+      password === Deno.env.get("SECRET_PASSWORD")
+    ) {
+      return new Response("Hello World!", {
+        headers: { "content-type": "text/plain" },
+      });
+    } else {
+      return new Response("You are not allowed to view this page", {
+        status: 403,
+      });
+    }
+  }
