a6fb6ceaa1e5a900805b932efe963c568c3d8bd8
Rick Cogley <rick.cogley@esolia.co.jp>
2022-07-05 07:42:05 +0900

    remove headers from index

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,21 +2,6 @@
 import { h } from "preact";
 import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
-import { Handlers } from "$fresh/server.ts";
-
-export const handler: Handlers = {
-  async GET(req, ctx) {
-    const resp = await ctx.render();
-    resp.headers.set("X-Custom-Header", "Hello");
-    resp.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-    resp.headers.set("X-Frame-Options", "SAMEORIGIN");
-    resp.headers.set("Referrer-Policy", "strict-origin");
-    resp.headers.set("X-Content-Type-Options", "nosniff");
-    resp.headers.set("X-Powered-By", "Blood Sweat Tears");
-    resp.headers.set("Permissions-Policy", "accelerometer=(), ambient-light-sensor=*, autoplay=(self), battery=(self), camera=(), cross-origin-isolated=*, fullscreen=*, geolocation=(self), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), usb=()");
-    return resp;
-  },
-};
 
 export default function Home() {
   return (
